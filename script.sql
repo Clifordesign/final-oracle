ef3716324eb116f4590884dab41f9bbe113a5534
Clifordesign <clifordesign@gmail.com>
2018-03-08 17:22:44 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,188 +1,203 @@
-create table malade(
-	no_malade int(3) primary key,
-	nom varchar2(15),
-	prenom varchar2(15),
-	adresse varchar2(15),
-	age int(3),
-	contre_indication varchar2(15),
-	date_entree(date),
-	no_chambre int(3),
-	no_batiment int (3),
-	constraint fk_malade  foreign key (no_chambre) references chambre(no_chambre),
-	constraint fk_malade2  foreign key (no_batiment) references chambre(no_batiment)
-);
+CREATE TABLE Fournisseur(
+	IdFournisseur varchar2(15) primary key,
+	NomFournisseur varchar2(15),
+	PrenomFournisseur varchar2(15),
+	PhoneFournisseur varchar(8),
+	AgeFournisseur number(3)
+    );
+    
+insert into Fournisseur values (01,'JULES','Wilfrid',47395748,40);
+insert into Fournisseur values (02,'LAZARD','Wilguy',33354263,20);
+insert into Fournisseur values (03,'ODNE','Nicolson',47395748,13);
+insert into Fournisseur values (04,'JULES','Wilfrid',47395748,40);
+insert into Fournisseur values (05,'FREE','Frida',48646285,40);
 
-create synonym patient 
-   for malade;
+SELECT * FROM fournisseur;
+    
+CREATE TABLE Categorie;(
+    IdCategorie number(5) primary key,
+    NomCategorie varchar2(15) check
+    (NomCategorie in ('Boissons','Condiments','laitiers','Viandes','poissons','Pâtes','céréales','Desserts','fruits','légumes')),
+    NomFournisseur varchar2(15)
+    );
+    
+insert into Categorie values (10,'Viandes',01);
+insert into Categorie values (2,'Poisson',02);
+insert into Categorie values (12,'céréales',03);
+insert into Categorie values (13,'Boissons',04);
 
-create table Plat(
-	nom varchar(15)
-);
+SELECT * FROM categorie;
 
-create synonym manger
-	for plat;
+CREATE TABLE Produit;(
+	IdProduit number(5) primary key,
+	NomProduit varchar2(15),
+	DateExpiration  Date,
+    IdCategorie number(5),
+    StockSecurite varchar2(15),
+    constraint fk_Produit foreign key (IdCategorie) references Categorie(IdCategorie)
+    );
+    
+    insert into Produit values(30,'MAIS',10,100);
+    insert into Produit values(31,'BANANE',(TO_DATE('12/3/2019','MM/DD/YYYY')),12,113);
+    insert into Produit values(32,'ORANGE',(TO_DATE('11/05/2099','MM/DD/YYYY')),13,113);
+    
+    SELECT * FROM PRODUIT;
+    
+    CREATE TABLE Client(
+	IdClient number(5) primary key,
+	NomClient varchar2(15),
+	PrenomClient varchar2(15),
+	DateNais Date,
+	VilleClient varchar2(15),
+	PhoneClient VARCHAR2(8)
+   );
+   
+    insert into Client values(40,'Diderot','Jeans',(to_date('1986/08/12 18:18:18','YYYY-MM-DD HH24:MI:SS')),'cap-haitien',38675458);
+    insert into Client values(41,'Josue','Jeans',(to_date('1996/08/26 12:18:08','YYYY-MM-DD HH24:MI:SS')),'Limbe',38675456);
+                                                
+    
+    SELECT * FROM client;
 
-create table repas(
-	date_repas(date),
-	type_repas varchar2(15),
-	no_malade int(3),
-	constraint fk_malade3 foreign key (no_malade) references malade(no_malade)
-);
+   
+CREATE TABLE Departement(
+    IdDepartement number(2) primary key,
+    NomDepartement varchar2(15) check (NomDepartement in('ACCOUNTING', 'OPERATIONS', 'RESEARCH', 'SALES')),
+    localDepartement varchar2(10)
+   );
+       
+    insert into Departement values (30, 'SALES','Nord');
+    insert into Departement values (10, 'ACCOUNTING','sud');
+    insert into Departement values (50, 'RESEARCH','est');
+    insert into Departement values (52, 'OPERATIONS','ouest');
+    insert into Departement values (55, 'RESEARCH','Centre');
+    
+    SELECT * FROM departement;
+       
+    	CREATE TABLE Employe(
+		IdEmploye number(4) primary key,
+		NomEmploye varchar2(10),
+		Fonction varchar2(9)check (Fonction in('CLERK', 'SALESMAN', 'PRESIDENT', 'MANAGER', 'ANALYST')),
+		CodeSuperieur number(4),
+		DateEmbauche Date,
+		Salaire number(7,2),
+		Commission number(7,2),
+		IdDepartement number(15),
+		Adresse varchar2(10),
+		constraint fk_Employe foreign key (IdDepartement) references Departement(IdDepartement)
+		);
+        
+                                            (TO_DATE('11/05/2099','MM/DD/YYYY'));
+    insert into Employe values (60,'Alce','Chef',111,'(TO_DATE('01/01/1981','MM/DD/YYYY'))',15000,200,50,'Vertiere');
+	insert into Employe values (61,'Black','pilote',113,01-01-1988,5000,'202',10,'Champin');
+	insert into Employe values (63,'Kenn','pilote',112,01-11-1988,500,'208',10,'charrier');
+	insert into Employe values (64,'Nico','pilote',114,08-11-1988,800,'209',30,'bell-air');
+	insert into Employe values (66,'Nini','SALESMAN',115,01-11-1981,15300,'229',30,'bell-Ance');
+	insert into Employe values (67,'Nina','ANALYST',215,05-11-1981,25300,'221',50,'Madeline');
+	insert into Employe values (68,'Cleck','Manager',216,05-11-1970,2300,'225',52,'ppls');
+	insert into Employe values (69,'Adlin','Chanteur',218,05-12-1981,1300,'226',10,'Arcule');
+	insert into Employe values (70,'ALLEN','ARTISTE',219,05-12-1980,3000,'227',10,'NOVION');
+	insert into Employe values (72,'MARTIN','pilote',220,05-12-1940,3080,'228',10,'HINCHE');
+	insert into Employe values (71,'Alix','Ouvrier',224,03-02-1949,280,'268',52,'Limbe');
+	insert into Employe values (73,'Cliford','Employe',227,03-02-1999,30290,'2890',50,'Milot');
+    
+    CREATE TABLE Commande(
+   	IdCommande number(5)primary key,
+   	DateCommande Date,
+   	DateLivraison Date,
+   	IdClient number(5),
+   	IdEmploye number(5),
+   	constraint fk_Commande foreign key (IdClient) references Client(IdClient),
+   	constraint fk_Commande2 foreign key (IdEmploye) references Employe (IdEmploye)
+    );
+    
+--    CREATE TABLE DetailCommande(
+--	IdCommande number(5),
+--	IdProduit number(5),
+--	Quantite number(5),
+--	PrixUnitaire number(5),
+--	constraint pk_DetailCommande primary key (IdCommande, IdProduit),
+--	--constraint fk_DetailCommande foreign key (IdCommande) references Commande(IdProduit),
+--    constraint fk_DetailCommande2 foreign key (IdProduit) references Produit(IdProduit)
+--    );
+--    
+    CREATE TABLE DetailCommande(
+	IdCommande number(5),
+	IdProduit number(5),
+	Quantite number(5),
+	PrixUnitaire number(5),
+	constraint pk_DetailCommande primary key(IdCommande,IdProduit),
+	constraint fk_DetailCommande2 foreign key (IdCommande) references Commande(IdCommande),
+	constraint fk_DetailCommande3 foreign key (IdProduit) references Produit(IdProduit)
 
-create synonym nourriture
-	for repas;
+	);
+    
+--    drop table produit;
+--    drop table departement;
+--    drop table employe;
+--    drop table client;
+--    drop table detailcommande;
+--    drop table commande;
+--    drop table fournisseur;
+--    drop table categorie;
 
-create table batiment(
-	no_batiment int(3) primary key
-);
+comment on table fournisseur
+    is 'la table des fournisseurs'; 
+    
+    comment on table categorie
+    is 'la table des categories';
+    
+comment on table produit
+    is 'la table des produits';
+    
+comment on table client
+    is 'la table des clients';
+    
+comment on table departement
+    is 'la table des departements';
+    
+comment on table employe
+    is 'la table des employes';
+    
+comment on table commande
+    is 'la table de commande';
+    
+comment on table detailcommande
+    is 'la table des details des commande';
+    
+    select * from hr.employees;
+    
+    -- INSERTION DES DONNEES--
 
-create synonym maison
-	for batiment;
 
-create table unite_fonctionnel(
-	no_unite int(3) primary key,
-	nom_unite varchar2(15)
-);
+ 
 
-create synonym unite
-	for unite_fonctionnel;
+    INSERT INTO produit (idproduit, nomproduit, dateexpiration,idcategorie, stocksecurite) VALUES (30,'MAIS','2018/12/13',2,100);
 
-create table chambre(
-	no_chambre int(3 primary key,
-	nb-list int(3),
-	no_batiment int(3),
-	no_unite int(3),
-	constraint fk_chambre foreign key (no_unite) references unite(no_unite)
-);
 
-create synonym appartement
-	for chambre;
-
-create table medecin(
-	no_medecin int(3) primary key,
-	nom_infirmiere varchar2(15),
-	no_unite
-	constraint fk_medecin foreign key (no_unite) references unite(no_unite)
-);
-
-create synonym docteur
-	for medecin;
-
-create table infirmiere(
-	nom_infirmiere varchar2(15),
-	no_unite int (5)
-	constraint fk_infirmiere foreign key (no_unite) references unite(no_unite)
-);
-
-create synonym assistante
-	for infirmiere;
-
-create table visite(
-	date_visite date,
-	commentaire varchar2(15),
-	no_malade int(3),
-	no_medecin int (3)
-	constraint fk_visite foreign key (no_malde) references malade(no_malde),
-	constraint fk_visite2 foreign key (no_medecin) references medecin(no_medecin)
-);
-
-create synonym social
-	for visite;
-
-create table analyse(
-	nom varchar2(15),
-	date_heure_prelevement date,
-	date_heure_expedition date,
-	date_heure_resultat date,
-	resultat varchar2(15),
-	date_visite date,
-	no_malade int(3),
-	nom_infirmiere int(3),
-	no_unite int (3),
-	constraint fk_analyse foreign key (date_visite) references visite(date_visite),
-	constraint fk_analyse2 foreign key (no_malade) references malade(no_malade),
-	constraint fk_analyse3 foreign key (nom_infirmiere) references infirmiere(nom_infirmiere),
-	constraint fk_analyse4 foreign key (no_unite) references unite(no_unite)
-);
-
-create synonym test
-	for analyse;
-
-create table acte_intervention(
-	heure_date date,
-	commentaire varchar2(15),
-	no_malade int(3),
-	no_infirmiere int (3),
-);
-
-create synonym operation
-	for acte_intervention;
-
-create table ordonnace(
-	no_ordonnace int(3) primary key,
-	date_heure_transmission date,
-	date_heure_reception date,
-	date_visite date,
-	no_malade int(3),
-	constraint fk_ordonnace foreign key (date_visite) references visite(date_visite),
-	constraint fk_ordonnace2 foreign key (no_malade) references malade(no_malade)
-);
-
-create synonym prescription
-	for ordonance;
-
-create table medicament(
-	reference_medicament varchar2(15) primary key,
-	inventaire_medicament int(5)
-);
-
-create synonym remede
-	for medicament;
-
-create table contre_indication(
-	nom varchar2(15),
-	commentaire varchar2(15),
-	reference_medicament varchar2(15),
-	fk_contre_indication foreign key (reference_medicament) references medicament(reference_medicament)
-);
-
-create synonym inconvenant
-	for contre_indication;
-
-create table ne_pas_servir(
-	no_malade int(3),
-	nom varchar2(15),
-	constraint fk_ne_pas_servir foreign key (no_malade) references malde (no_malade),
-	constraint fk_ne_pas_servir2 foreign key (nom) references malade (nom)
-);
-
-create synonym danger
-	for ne_pas_servir
-
-create table compostion(
-
-);
-
-create synonym contenu
-	for compostion;
-
-create table intervention_prescrite(
-	date_visite date,
-	no_malade int(3),
-	no_unite int(3),
-	commentaire varchar2(15),
-	date_heure_prevue date,
-	date_heure_effective date,
-	resultat varchar2(15)
-	constraint fk_intervention_prescrite foreign key (date_visite) references visite (date_visite),
-	constraint fk_interventionPrescrite2 foreign key (no_malade) references malade (no_malade),
-	constraint fk_intervention_prescrite2 foreign key (no_unite) references unite(no_unite)
-);
-
-create synonym operation_prescrite
-	for intervention_prescrite;
-
-create table ligne_ordonance(
-	
-):
+        
+        select * from produit;
+        
+            
+            	insert into Employe values (60,'Alce','Chef',111,01-01-1981,15000,'200',50,'Vertiere');
+	insert into Employe values (61,'Black','pilote',113,01-01-1988,5000,'202',10,'Champin');
+	insert into Employe values (63,'Kenn','pilote',112,01-11-1988,500,'208',10,'charrier');
+	insert into Employe values (64,'Nico','pilote',114,08-11-1988,800,'209',30,'bell-air');
+	insert into Employe values (66,'Nini','SALESMAN',115,01-11-1981,15300,'229',30,'bell-Ance');
+	insert into Employe values (67,'Nina','ANALYST',215,05-11-1981,25300,'221',50,'Madeline');
+	insert into Employe values (68,'Cleck','Manager',216,05-11-1970,2300,'225',52,'ppls');
+	insert into Employe values (69,'Adlin','Chanteur',218,'1981-05-12',1300,'226',10,'Arcule');
+	insert into Employe values (70,'ALLEN','ARTISTE',219,05-12-1980,3000,'227',10,'NOVION');
+	insert into Employe values (72,'MARTIN','pilote',220,05-12-1940,3080,'228',10,'HINCHE');
+	insert into Employe values (71,'Alix','Ouvrier',224,03-02-1949,280,'268',52,'Limbe');
+	insert into Employe values (73,'Cliford','Employe',227,03-02-1999,30290,'2890',50,'Milot');
+    
+    		insert into Commande values (80,12-06-2017,09-05-2017,42,68);
+		insert into Commande values (81,'2018/06/03',29-03-2018,40,60);
+		insert into Commande values (82,06-03-2018,30-03-2018,41,61);
+        
+        	insert into DetailCommande values (80,30,6,50);	
+	insert into DetailCommande values (81,31,8,100);
+	insert into DetailCommande values (82,32,3,150);
+    
+    
 

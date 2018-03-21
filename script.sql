0b046751a04d6044ccc7d2a5b0d37a39d2520db6
Clifordesign <clifordesign@gmail.com>
2018-03-21 08:09:57 -0400

    Update script.sql

--- script.sql
+++ script.sql
@@ -86,19 +86,21 @@ CREATE TABLE Departement(
 		constraint fk_Employe foreign key (IdDepartement) references Departement(IdDepartement)
 		);
         
-                                            (TO_DATE('11/05/2099','MM/DD/YYYY'));
-    insert into Employe values (60,'Alce','Chef',111,'(TO_DATE('01/01/1981','MM/DD/YYYY'))',15000,200,50,'Vertiere');
-	insert into Employe values (61,'Black','pilote',113,01-01-1988,5000,'202',10,'Champin');
-	insert into Employe values (63,'Kenn','pilote',112,01-11-1988,500,'208',10,'charrier');
-	insert into Employe values (64,'Nico','pilote',114,08-11-1988,800,'209',30,'bell-air');
-	insert into Employe values (66,'Nini','SALESMAN',115,01-11-1981,15300,'229',30,'bell-Ance');
-	insert into Employe values (67,'Nina','ANALYST',215,05-11-1981,25300,'221',50,'Madeline');
-	insert into Employe values (68,'Cleck','Manager',216,05-11-1970,2300,'225',52,'ppls');
-	insert into Employe values (69,'Adlin','Chanteur',218,05-12-1981,1300,'226',10,'Arcule');
-	insert into Employe values (70,'ALLEN','ARTISTE',219,05-12-1980,3000,'227',10,'NOVION');
-	insert into Employe values (72,'MARTIN','pilote',220,05-12-1940,3080,'228',10,'HINCHE');
-	insert into Employe values (71,'Alix','Ouvrier',224,03-02-1949,280,'268',52,'Limbe');
-	insert into Employe values (73,'Cliford','Employe',227,03-02-1999,30290,'2890',50,'Milot');
+                                           
+    insert into Employe values (60,'Alce','CLERK',111,(TO_DATE('01/01/1981','MM/DD/YYYY')),15000,200,50,'Vertiere');
+	insert into Employe values (61,'Black','SALESMAN',113,(TO_DATE('01-01-1988','MM-DD-YYYY')),5000,'202',10,'Champin');
+	insert into Employe values (63,'Kenn','PRESIDENT',112,(TO_DATE('01-11-1988','MM-DD-YYYY')),500,'208',10,'charrier');
+	insert into Employe values (64,'Nico','SALESMAN',114,(TO_DATE('08-11-1988','MM-DD-YYYY')),800,'209',30,'bell-air');
+	insert into Employe values (66,'Nini','SALESMAN',115,(TO_DATE('01-11-1981','MM-DD-YYYY')),15300,'229',30,'bell-Ance');
+	insert into Employe values (67,'Nina','ANALYST',215,(TO_DATE('05-11-1981','MM-DD-YYYY')),25300,'221',50,'Madeline');
+	insert into Employe values (68,'Cleck','MANAGER',216,(TO_DATE('05-11-1970','MM-DD-YYYY')),2300,'225',52,'ppls');
+	insert into Employe values (69,'Adlin','CLERK',218,(TO_DATE('05-12-1981','MM-DD-YYYY')),1300,'226',10,'Arcule');
+	insert into Employe values (70,'ALLEN','ANALYST',219,(TO_DATE('05-12-1980','MM-DD-YYYY')),3000,'227',10,'NOVION');
+	insert into Employe values (72,'MARTIN','PRESIDENT',220,(TO_DATE('05-12-1940','MM-DD-YYYY')),3080,'228',10,'HINCHE');
+	insert into Employe values (71,'Alix','ANALYST',224,(TO_DATE('03-02-1949','MM-DD-YYYY')),280,'268',52,'Limbe');
+	insert into Employe values (73,'Cliford','ANALYST',227,(TO_DATE('03-02-1999','MM-DD-YYYY')),30290,'2890',50,'Milot');
+    
+    SELECT * FROM employe;
     
     CREATE TABLE Commande(
    	IdCommande number(5)primary key,
@@ -110,16 +112,13 @@ CREATE TABLE Departement(
    	constraint fk_Commande2 foreign key (IdEmploye) references Employe (IdEmploye)
     );
     
---    CREATE TABLE DetailCommande(
---	IdCommande number(5),
---	IdProduit number(5),
---	Quantite number(5),
---	PrixUnitaire number(5),
---	constraint pk_DetailCommande primary key (IdCommande, IdProduit),
---	--constraint fk_DetailCommande foreign key (IdCommande) references Commande(IdProduit),
---    constraint fk_DetailCommande2 foreign key (IdProduit) references Produit(IdProduit)
---    );
---    
+        insert into Commande values (80,(TO_DATE('12-06-2017','DD-MM-YYYY')),TO_DATE('09-05-2017','DD-MM-YYYY'),40,68);
+		insert into Commande values (81,TO_DATE('06-03-2018','DD-MM-YYYY'),TO_DATE('29-03-2018','DD-MM-YYYY'),40,60);
+		insert into Commande values (82,TO_DATE('06-03-2018','DD-MM-YYYY'),TO_DATE('30-03-2018','DD-MM-YYYY'),41,61);
+        
+        SELECT * FROM commande;
+    
+        
     CREATE TABLE DetailCommande(
 	IdCommande number(5),
 	IdProduit number(5),
@@ -128,17 +127,15 @@ CREATE TABLE Departement(
 	constraint pk_DetailCommande primary key(IdCommande,IdProduit),
 	constraint fk_DetailCommande2 foreign key (IdCommande) references Commande(IdCommande),
 	constraint fk_DetailCommande3 foreign key (IdProduit) references Produit(IdProduit)
-
-	);
+    );
     
---    drop table produit;
---    drop table departement;
---    drop table employe;
---    drop table client;
---    drop table detailcommande;
---    drop table commande;
---    drop table fournisseur;
---    drop table categorie;
+	insert into DetailCommande values (80,30,6,50);	
+	insert into DetailCommande values (81,31,8,100);
+	insert into DetailCommande values (82,32,3,150);
+    
+    SELECT * FROM DetailCommande;
+    
+    --COMMENTAIRES
 
 comment on table fournisseur
     is 'la table des fournisseurs'; 
@@ -164,40 +161,124 @@ comment on table commande
 comment on table detailcommande
     is 'la table des details des commande';
     
-    select * from hr.employees;
+    select * from employe;
     
-    -- INSERTION DES DONNEES--
-
-
- 
-
-    INSERT INTO produit (idproduit, nomproduit, dateexpiration,idcategorie, stocksecurite) VALUES (30,'MAIS','2018/12/13',2,100);
-
-
-        
-        select * from produit;
-        
-            
-            	insert into Employe values (60,'Alce','Chef',111,01-01-1981,15000,'200',50,'Vertiere');
-	insert into Employe values (61,'Black','pilote',113,01-01-1988,5000,'202',10,'Champin');
-	insert into Employe values (63,'Kenn','pilote',112,01-11-1988,500,'208',10,'charrier');
-	insert into Employe values (64,'Nico','pilote',114,08-11-1988,800,'209',30,'bell-air');
-	insert into Employe values (66,'Nini','SALESMAN',115,01-11-1981,15300,'229',30,'bell-Ance');
-	insert into Employe values (67,'Nina','ANALYST',215,05-11-1981,25300,'221',50,'Madeline');
-	insert into Employe values (68,'Cleck','Manager',216,05-11-1970,2300,'225',52,'ppls');
-	insert into Employe values (69,'Adlin','Chanteur',218,'1981-05-12',1300,'226',10,'Arcule');
-	insert into Employe values (70,'ALLEN','ARTISTE',219,05-12-1980,3000,'227',10,'NOVION');
-	insert into Employe values (72,'MARTIN','pilote',220,05-12-1940,3080,'228',10,'HINCHE');
-	insert into Employe values (71,'Alix','Ouvrier',224,03-02-1949,280,'268',52,'Limbe');
-	insert into Employe values (73,'Cliford','Employe',227,03-02-1999,30290,'2890',50,'Milot');
+    -- REPONSE 1
     
-    		insert into Commande values (80,12-06-2017,09-05-2017,42,68);
-		insert into Commande values (81,'2018/06/03',29-03-2018,40,60);
-		insert into Commande values (82,06-03-2018,30-03-2018,41,61);
-        
-        	insert into DetailCommande values (80,30,6,50);	
-	insert into DetailCommande values (81,31,8,100);
-	insert into DetailCommande values (82,32,3,150);
-    
-    
+    SELECT idemploye, nomemploye FROM employe WHERE dateembauche BETWEEN TO_DATE('1981-01-01', 'YYYY-MM-DD')
+                    AND TO_DATE('1981-12-31', 'YYYY-MM-DD');
+                    
+-- 134
+SELECT fonction, salaire FROM employe ORDER BY fonction HAVING salaire=
+(SELECT AVG salaire) FROM employe ORDER BY fonction);
 
+--22
+SELECT iddepartement FROM employe WHERE salaire =
+(SELECT MAX(salaire) FROM employe GROUP BY iddepartement);
+                    
+--9
+SELECT * FROM employe WHERE fonction = (SELECT fonction FROM employe WHERE 
+iddepartement.departement = iddepartement.employe WHERE nomemploye = 'martin');
+
+--2
+SELECT * FROM employe WHERE;
+
+--6
+SELECT nomemploye FROM employe WHERE
+dateembauche = (SELECT MIN(dateembauche) FROM employe);
+
+--7
+SELECT * FROM employe WHERE 
+salaire > (SELECT AVG(salaire) FROM employe);
+
+--10
+--22
+select iddepartement, count(*) "Nombre employe"from employe
+group by iddepartement;
+
+select nomemploye, iddepartement, salaire from employe 
+   where (salaire, iddepartement) in 
+     (select MIN(salaire), iddepartement from employe group by iddepartement); 
+     
+     --18
+     select iddepartement from employe 
+group by iddepartement 
+having count(*) > (select count(*) from employe where iddepartement=10);
+
+--22
+select nomemploye, salaire from employe
+   where salaire = 
+     (select max(salaire) from employe where
+     iddepartement=30); 
+     
+     select nomemploye, salaire from employe
+   where salaire = 
+     (select max(salaire) from employe);
+     
+     select iddepartement from departement
+where iddepartement not in
+  (select iddepartement from employe
+   where salaire > 10000);
+   
+   --2
+SELECT * from employe;
+FROM (;
+SELECT departement MAX(fonction) FROM employe;
+
+select iddepartement from departement where
+(select max(fonction) from employe where
+fonction = 'pilote');
+
+--3
+SELECT * FROM employe  WHERE
+fonction = 'SALESMAN' OR fonction = 'ANALYST' WHERE
+(SELECT dateembauche BETWEEN TO_DATE('01-01-1981', 'DD-MM-YYYY') AND TO_DATE('31-12-1981', 'DD-MM-YYYY'));
+AND salaire > 12000;
+
+--4
+SELECT * FROM employe
+WHERE   fonction = 'SALESMAN' AND
+salaire > (SELECT MIN(salaire) FROM employe WHERE fonction = 'CLERK');
+
+--5
+SELECT COUNT(*) FROM employe GROUP BY TO_CHAR(dateembauche,'YYYY');
+
+--8
+SELECT nomemploye, salaire FROM employe WHERE salaire >
+(SELECT salaire FROM employe WHERE  nomemploye = 'ALLEN');
+
+--9
+SELECT * FROM employe WHERE fonction =
+(SELECT fonction FROM employe WHERE nomemploye = 'MARTIN')
+AND iddepartement = (SELECT iddepartement FROM employe WHERE nomemploye = 'MARTIN');
+
+--12
+SELECT iddepartement FROM departement WHERE
+(SELECT AVG(salaire) from employe) > (SELECT AVG(salaire) FROM employe WHERE iddepartement = 30);
+
+SELECT fonction FROM employe WHERE ;
+
+(SELECT AVG(salaire) FROM employe WHERE ;
+(SELECT fonction, salaire FROM employe WHERE fonction IS );
+
+
+--25
+
+SELECT nomemploye, codesuperieur as nomchef FROM employe;
+
+select * from departement where iddepartement not in (select iddepartement from Employe);
+
+SELECT fonction, AVG(Salaire) "salaire moyenne"
+             FROM Employe
+            GROUP BY fonction;
+
+
+
+
+
+
+select avg(salaire) from employe;
+
+
+
+SELECT * FROM employe;
